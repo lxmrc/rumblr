8f7a903277d25f2bde139a9a50266d0c9ff25dc1
Alex Mercea <alex@lxmrc.com>
2021-04-04 13:51:30 +1000

    Improve user log in spec

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -5,10 +5,12 @@ RSpec.feature "User log in", type: :feature do
                                  email: "user@example.com", 
                                  username: "test-user") }
 
-  scenario "successfully" do
+  before do
     visit root_path
     click_link "Log in"
+  end
 
+  scenario "with valid details" do
     fill_in "Email", with: "user@example.com"
     fill_in "user_password", with: "password"
     click_button "Log in"
@@ -18,5 +20,21 @@ RSpec.feature "User log in", type: :feature do
     expect(page).to have_content "Edit profile"
     expect(page).to have_content "New Post"
     expect(page).to have_content "Log out"
+  end
+
+  scenario "with invalid email" do
+    fill_in "Email", with: "invalid_user@example.com"
+    fill_in "user_password", with: "password"
+    click_button "Log in"
+
+    expect(page).to have_content "Invalid Email or password."
+  end
+
+  scenario "with invalid password" do
+    fill_in "Email", with: "user@example.com"
+    fill_in "user_password", with: "incorrect password"
+    click_button "Log in"
+
+    expect(page).to have_content "Invalid Email or password."
   end
 end
