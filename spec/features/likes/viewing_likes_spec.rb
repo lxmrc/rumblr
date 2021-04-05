8c5acadf94d4cfd16aefa1a0b5c7cff8d50402da
Alex Mercea <alex@lxmrc.com>
2021-04-05 14:24:38 +1000

    Add viewing likes spec

---
+++ spec/features/likes/viewing_likes_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "Users can see likes", type: :feature do
+  let(:author) { FactoryBot.create(:user) }
+  let(:liker) { FactoryBot.create(:user) }
+  let!(:post) { FactoryBot.create(:post, author: author) }
+  let!(:like) { FactoryBot.create(:like, user: liker, post: post) }
+
+  scenario "on a post's page" do
+    visit post_path(post)
+
+    expect(page).to have_content("#{liker} liked this.")
+  end
+end
