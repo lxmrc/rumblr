73f9e2007464f41662ee538c61f067c6d0843318
Alex Mercea <alex@lxmrc.com>
2021-04-21 15:43:45 +1000

    Improve updating profiles spec

--- spec/features/users/updating_profiles_spec.rb
+++ spec/features/users/updating_profiles_spec.rb
@@ -17,7 +17,22 @@ RSpec.feature "Users can edit their profiles", type: :feature do
     expect(page).to have_content "Your account has been updated successfully."
 
     visit edit_user_registration_path(user)
+
     expect(page).to have_field("Email", with: "different_email@example.com")
+
+    click_link "Log out"
+    click_link "Log in"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_button "Log in"
+
+    expect(page).to have_content "Invalid Email or password."
+
+    fill_in "Email", with: "different_email@example.com"
+    fill_in "Password", with: "password"
+    click_button "Log in"
+
+    expect(page).to have_content "Signed in successfully."
   end
 
   scenario "password" do
@@ -30,9 +45,14 @@ RSpec.feature "Users can edit their profiles", type: :feature do
 
     click_link "Log out"
     click_link "Log in"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    click_button "Log in"
+
+    expect(page).to have_content "Invalid Email or password."
 
     fill_in "Email", with: "user@example.com"
-    fill_in "user_password", with: "different password"
+    fill_in "Password", with: "different password"
     click_button "Log in"
 
     expect(page).to have_content "Signed in successfully."
@@ -43,22 +63,28 @@ RSpec.feature "Users can edit their profiles", type: :feature do
     click_button "Update Account"
 
     expect(page).to have_content "Your account has been updated successfully."
+
+    click_link "View profile"
     expect(page).to have_content "different-username"
+    expect(page).to have_current_path "/users/different-username"
   end
 
   scenario "bio" do
-    fill_in "Bio", with: "I have a bio now."
+    fill_in "Bio", with: "In West Philadelphia born and raised..."
     click_button "Update Account"
 
     expect(page).to have_content "Your account has been updated successfully."
-    expect(page).to have_content "I have a bio now."
+    expect(page).to have_content "In West Philadelphia born and raised..."
   end
 
   scenario "profile picture" do
-    attach_file("Profile picture", Rails.root + "spec/fixtures/files/test.png")
+    test_png_path = Rails.root + "spec/fixtures/files/test.png"
+    attach_file("Profile picture", test_png_path)
     click_button "Update Account"
 
+    uploaded_png_path = page.find("#profile-picture")["src"]
     expect(page).to have_content "Your account has been updated successfully."
-    expect(page.find("#profile-picture")["src"]).to have_content "test.png"
+    # TODO: Check whether files are actually the same.
+    # expect(uploaded_png_path).to be_same_file_as(test_png_path)
   end
 end
