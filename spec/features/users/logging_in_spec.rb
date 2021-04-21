9df097ad522fcad33a9bc9f50897f7a23e697d91
Alex Mercea <alex@lxmrc.com>
2021-04-21 14:56:39 +1000

    Improve logging in spec

--- spec/features/users/logging_in_spec.rb
+++ spec/features/users/logging_in_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "User log in", type: :feature do
+RSpec.feature "Users can log in", type: :feature do
   let!(:user) {
     FactoryBot.create(:user,
       email: "user@example.com",
@@ -14,19 +14,20 @@ RSpec.feature "User log in", type: :feature do
 
   scenario "with valid details" do
     fill_in "Email", with: "user@example.com"
-    fill_in "user_password", with: "password"
+    fill_in "Password", with: "password"
     click_button "Log in"
 
     expect(page).to have_content "Signed in successfully."
-    expect(page).to have_content "test-user"
-    expect(page).to have_content "Edit profile"
-    expect(page).to have_content "New Post"
-    expect(page).to have_content "Log out"
+    expect(page).to have_content user.username
+    expect(page).to have_link "New post"
+    expect(page).to have_link "View profile"
+    expect(page).to have_link "Edit profile"
+    expect(page).to have_link "Log out"
   end
 
   scenario "with invalid email" do
     fill_in "Email", with: "invalid_user@example.com"
-    fill_in "user_password", with: "password"
+    fill_in "Password", with: "password"
     click_button "Log in"
 
     expect(page).to have_content "Invalid Email or password."
