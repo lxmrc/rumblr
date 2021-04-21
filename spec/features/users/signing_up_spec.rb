63ef5e66e21ac0fabea75c9ee6b178175881a86b
Alex Mercea <alex@lxmrc.com>
2021-04-21 14:53:46 +1000

    Improve signing up spec

--- spec/features/users/signing_up_spec.rb
+++ spec/features/users/signing_up_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "User registration", type: :feature do
+RSpec.feature "Users can sign up", type: :feature do
   before do
     visit root_path
     click_link "Sign up"
@@ -8,27 +8,27 @@ RSpec.feature "User registration", type: :feature do
 
   scenario "with valid input" do
     fill_in "Email", with: "user@example.com"
-    fill_in "Username", with: "valid-username69"
-    fill_in "user_password", with: "password"
-    fill_in "user_password_confirmation", with: "password"
+    fill_in "Username", with: "valid-username99"
+    fill_in "Password", with: "password", match: :prefer_exact
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
 
     expect(page).to have_content "Welcome! Complete your profile below."
-    expect(page).to have_content "New post"
-    expect(page).to have_content "View profile"
-    expect(page).to have_content "Edit profile"
-    expect(page).to have_content "Log out"
-
-    expect(page).to have_field("user[username]", with: "valid-username69")
+    expect(page).to have_field("user[username]", with: "valid-username99")
     expect(page).to have_field "user[profile_picture]"
     expect(page).to have_field "user[bio]"
+
+    expect(page).to have_link "New post"
+    expect(page).to have_link "View profile"
+    expect(page).to have_link "Edit profile"
+    expect(page).to have_link "Log out"
   end
 
   scenario "with invalid email" do
     fill_in "Email", with: "not an email address"
-    fill_in "Username", with: "valid-username69"
-    fill_in "user_password", with: "password"
-    fill_in "user_password_confirmation", with: "password"
+    fill_in "Username", with: "valid-username99"
+    fill_in "Password", with: "password", match: :prefer_exact
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
 
     expect(page).to have_content "Email is invalid"
@@ -36,9 +36,9 @@ RSpec.feature "User registration", type: :feature do
 
   scenario "with missing email" do
     fill_in "Email", with: ""
-    fill_in "Username", with: "valid-username69"
-    fill_in "user_password", with: "password"
-    fill_in "user_password_confirmation", with: "password"
+    fill_in "Username", with: "valid-username99"
+    fill_in "Password", with: "password", match: :prefer_exact
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
 
     expect(page).to have_content "Email can't be blank"
@@ -46,17 +46,17 @@ RSpec.feature "User registration", type: :feature do
 
   scenario "with email already taken" do
     fill_in "Email", with: "user@example.com"
-    fill_in "Username", with: "valid-username69"
-    fill_in "user_password", with: "password"
-    fill_in "user_password_confirmation", with: "password"
+    fill_in "Username", with: "valid-username99"
+    fill_in "Password", with: "password", match: :prefer_exact
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     click_link "Log out"
     click_link "Sign up"
 
     fill_in "Email", with: "user@example.com"
-    fill_in "Username", with: "valid-username69"
-    fill_in "user_password", with: "password"
-    fill_in "user_password_confirmation", with: "password"
+    fill_in "Username", with: "different-username99"
+    fill_in "Password", with: "password", match: :prefer_exact
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
 
     expect(page).to have_content "Email has already been taken"
@@ -65,8 +65,8 @@ RSpec.feature "User registration", type: :feature do
   scenario "with special characters in username" do
     fill_in "Email", with: "user@example.com"
     fill_in "Username", with: "invalid @#$%^&* username"
-    fill_in "user_password", with: "password"
-    fill_in "user_password_confirmation", with: "password"
+    fill_in "Password", with: "password", match: :prefer_exact
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
 
     expect(page).to have_content "Username is invalid"
@@ -75,8 +75,8 @@ RSpec.feature "User registration", type: :feature do
   scenario "with username longer than 32 characters" do
     fill_in "Email", with: "user@example.com"
     fill_in "Username", with: "usernamelongerthanthirtytwocharacters"
-    fill_in "user_password", with: "password"
-    fill_in "user_password_confirmation", with: "password"
+    fill_in "Password", with: "password", match: :prefer_exact
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
 
     expect(page).to have_content "Username is too long (maximum is 32 characters)"
@@ -84,17 +84,17 @@ RSpec.feature "User registration", type: :feature do
 
   scenario "with username already taken" do
     fill_in "Email", with: "user@example.com"
-    fill_in "Username", with: "valid-username69"
-    fill_in "user_password", with: "password"
-    fill_in "user_password_confirmation", with: "password"
+    fill_in "Username", with: "valid-username99"
+    fill_in "Password", with: "password", match: :prefer_exact
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     click_link "Log out"
     click_link "Sign up"
 
     fill_in "Email", with: "different_user@example.com"
-    fill_in "Username", with: "valid-username69"
-    fill_in "user_password", with: "password"
-    fill_in "user_password_confirmation", with: "password"
+    fill_in "Username", with: "valid-username99"
+    fill_in "Password", with: "password", match: :prefer_exact
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
 
     expect(page).to have_content "Username has already been taken"
